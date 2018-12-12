56abc1eeefd24ba6136b8f958acdccba244dfa61
Zetinator <erick_zetina@hotmail.com>
2018-12-12 00:27:00 -0600

    parallel threading working...

--- src/scheduler_fcfs.py
+++ src/scheduler_fcfs.py
@@ -23,51 +23,56 @@ class DerThread (threading.Thread):
         self.threadID = threadID
         self.process = process
     def run(self):
-        print ("Starting " + self.name)
+        # print ("Starting " + self.name)
+        # print("threadID --> " + str(self.threadID))
+        # print("status   --> " + str(process.current_state))
         # process.run()
+        # process.ready()
         # Get lock to synchronize threads
         threadLock.acquire()
-        current_status[threadID] = process.current_state
+        current_status[self.threadID] = process.current_state
+        # print(str(current_status))
         # Free lock to release next thread
         threadLock.release()
 
 
 class Scheduler:
     def __init__(self):
-
-        # for process in processes:
-            # print("ahi va")
+        print("---------Ready--------")
+        for i, process in enumerate(processes):
+            process.ready()
+        print("\n")
 
         # state variables
         self.the_one_running = None
         self.waiting_list = []
 
 
-def main(args):
-    # initialize processes
+if __name__ == '__main__':
     threadLock = threading.Lock()
+    # initialize processes
     loader = ProcessLoader()
+    print("-----Initializing-----")
     processes = loader.load()
-    current_status = []
+    print("\n")
 
+    # status vector
+    current_status = []
     for i, process in enumerate(processes):
         current_status.append(None)
-        print("si esta bien --> " + str(i))
 
     # initialize scheduler
     scheduler = Scheduler()
-    threads = []
 
     # Create new threads
+    threads = []
     for i, process in enumerate(processes):
         thread = DerThread(i, process)
-        thread = start()
+        thread.start()
         threads.append(thread)
 
     # Wait for all threads to complete
     for t in threads:
         t.join()
+    print("the... "  + str(current_status))
     print ("Exiting Main Thread")
-
-if __name__ == '__main__':
-    main(sys.argv)
